469f8eff9440390824ecde4349c0a37341383a41
Karolis Ramanauskas <karolisr@gmail.com>
2026-04-01 20:42:34 -0500

    - Updates Rust config files. - Formatting.

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,7 @@ mod translation;
 
 pub use complementation::{complement, rev_comp, reverse};
 pub use genetic_code::{
-    get_gen_code_name, get_start_codons, get_stop_codons, get_trans_table, validate_gc_id,
+    get_gen_code_name, get_start_codons, get_stop_codons, get_trans_table,
+    validate_gc_id,
 };
 pub use translation::translate;
